b16a3a7f4c8c6226150eca1b649f9e6acc496741
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2023-05-11 21:19:38 -0400

    align build with extra-icons

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
 import com.github.benmanes.gradle.versions.reporter.PlainTextReporter
 import com.github.benmanes.gradle.versions.reporter.result.Result
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import com.palantir.gradle.gitversion.VersionDetails
+import groovy.lang.Closure
+import org.jetbrains.changelog.Changelog
 
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.13.3" // https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.changelog") version "2.0.0" // https://github.com/JetBrains/gradle-changelog-plugin
     id("com.github.ben-manes.versions") version "0.46.0" // https://github.com/ben-manes/gradle-versions-plugin
+    id("com.adarshr.test-logger") version "3.2.0" // https://github.com/radarsh/gradle-test-logger-plugin
+    id("com.palantir.git-version") version "3.0.0" // https://github.com/palantir/gradle-git-version
 }
 
 // Import variables from gradle.properties file
@@ -14,20 +21,29 @@ val pluginDownloadIdeaSources: String by project
 val pluginInstrumentPluginCode: String by project
 val pluginVersion: String by project
 val pluginJavaVersion: String by project
-val pluginEnableBuildSearchableOptions: String by project
+val testLoggerStyle: String by project
 
-val inCI = System.getenv("CI") != null
+version = if (pluginVersion == "auto") {
+    val versionDetails: Closure<VersionDetails> by extra
+    val lastTag = versionDetails().lastTag
+    if (lastTag.startsWith("v", ignoreCase = true)) {
+        lastTag.substring(1)
+    } else {
+        lastTag
+    }
+} else {
+    pluginVersion
+}
 
-val junitVersion = "5.8.2"
-
-logger.quiet("Will use IDEA $pluginIdeaVersion and Java $pluginJavaVersion")
+logger.quiet("Will use IDEA $pluginIdeaVersion and Java $pluginJavaVersion. Plugin version set to $version.")
 
 group = "lermitage.intellij.extratci"
-version = pluginVersion
 
 repositories {
     mavenCentral()
 }
+
+val junitVersion = "5.9.3"
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
@@ -35,12 +51,28 @@ dependencies {
 }
 
 intellij {
-    downloadSources.set(pluginDownloadIdeaSources.toBoolean() && !inCI)
-    instrumentCode.set(pluginInstrumentPluginCode.toBoolean())
+    downloadSources.set(pluginDownloadIdeaSources.toBoolean() && !System.getenv().containsKey("CI"))
+    instrumentCode.set(true)
     pluginName.set("Extra Toolwindow Colorful Icons")
-    sandboxDir.set("${rootProject.projectDir}/.idea-sandbox/${shortIdeVersion(pluginIdeaVersion)}")
+    sandboxDir.set("${rootProject.projectDir}/.idea-sandbox/${shortenIdeVersion(pluginIdeaVersion)}")
     updateSinceUntilBuild.set(false)
     version.set(pluginIdeaVersion)
+}
+
+changelog {
+    headerParserRegex.set("(.*)".toRegex())
+    itemPrefix.set("*")
+}
+
+testlogger {
+    try {
+        theme = ThemeType.valueOf(testLoggerStyle)
+    } catch (e: Exception) {
+        theme = ThemeType.PLAIN
+        logger.warn("Invalid testLoggerRichStyle value '$testLoggerStyle', " +
+            "will use PLAIN style instead. Accepted values are PLAIN, STANDARD and MOCHA.")
+    }
+    showSimpleNames = true
 }
 
 tasks {
@@ -48,6 +80,7 @@ tasks {
         sourceCompatibility = pluginJavaVersion
         targetCompatibility = pluginJavaVersion
         options.compilerArgs = listOf("-Xlint:deprecation")
+        options.encoding = "UTF-8"
     }
     withType<Test> {
         useJUnitPlatform()
@@ -69,12 +102,22 @@ tasks {
         }
     }
     runIde {
-        jvmArgs("-Xms128m")
-        jvmArgs("-Xmx1024m")
+        maxHeapSize = "1g" // https://docs.gradle.org/current/dsl/org.gradle.api.tasks.JavaExec.html
+
+        // force detection of slow operations in EDT when playing with sandboxed IDE (SlowOperations.assertSlowOperationsAreAllowed)
+        jvmArgs("-Dide.slow.operations.assertion=true")
+
+        autoReloadPlugins.set(false)
     }
     buildSearchableOptions {
-        enabled = pluginEnableBuildSearchableOptions.toBoolean()
-        jvmArgs = listOf("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+        enabled = false
+    }
+    patchPluginXml {
+        changeNotes.set(provider {
+            with(changelog) {
+                renderItem(getLatest(), Changelog.OutputType.HTML)
+            }
+        })
     }
 }
 
@@ -89,12 +132,15 @@ fun isNonStable(version: String): Boolean {
 
 /** Return an IDE version string without the optional PATCH number.
  * In other words, replace IDE-MAJOR-MINOR(-PATCH) by IDE-MAJOR-MINOR. */
-fun shortIdeVersion(version: String): String {
+fun shortenIdeVersion(version: String): String {
+    if (version.contains("SNAPSHOT", ignoreCase = true)) {
+        return version
+    }
     val matcher = Regex("[A-Za-z]+[\\-]?[0-9]+[\\.]{1}[0-9]+")
     return try {
         matcher.findAll(version).map { it.value }.toList()[0]
     } catch (e: Exception) {
-        logger.warn("Failed to shorten IDE version $version", e)
+        logger.warn("Failed to shorten IDE version $version: ${e.message}")
         version
     }
 }
