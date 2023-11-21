9ef73089bd381200a8621afc5c2517bde246a3d3
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2023-11-21 01:29:16 +0100

    upgrade gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ import javax.xml.xpath.XPathFactory
 
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.15.0" // https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.intellij") version "1.16.0" // https://github.com/JetBrains/gradle-intellij-plugin
     id("org.jetbrains.changelog") version "2.2.0" // https://github.com/JetBrains/gradle-changelog-plugin
     id("com.github.ben-manes.versions") version "0.47.0" // https://github.com/ben-manes/gradle-versions-plugin
     id("com.palantir.git-version") version "3.0.0" // https://github.com/palantir/gradle-git-version
